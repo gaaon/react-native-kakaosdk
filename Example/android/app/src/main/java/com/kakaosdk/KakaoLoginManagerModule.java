31f1c1aee80d416d0d04d4c91245ed3294812f13
Taewoo Kim <xodn4195@gmail.com>
2016-08-30 02:15:16 +0900

    Add kakao login button

--- Example/android/app/src/main/java/com/kakaosdk/KakaoLoginManagerModule.java
+++ Example/android/app/src/main/java/com/kakaosdk/KakaoLoginManagerModule.java
@@ -1,15 +1,22 @@
 package com.kakaosdk;
 
 import android.content.Intent;
+import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
 import com.kakao.auth.AuthType;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
 import com.kakao.util.exception.KakaoException;
 import com.kakao.util.helper.StoryProtocol;
 import com.kakao.util.helper.TalkProtocol;
@@ -45,14 +52,16 @@ public class KakaoLoginManagerModule extends ReactContextBaseJavaModule implemen
 
     private List<AuthType> getAuthTypes() {
         final List<AuthType> availableAuthTypes = new ArrayList<AuthType>();
+        availableAuthTypes.add(AuthType.KAKAO_ACCOUNT);
+        /*
         if(TalkProtocol.existCapriLoginActivityInTalk(getReactApplicationContext(), Session.getCurrentSession().isProjectLogin())){
             availableAuthTypes.add(AuthType.KAKAO_TALK);
-            availableAuthTypes.add(AuthType.KAKAO_TALK_EXCLUDE_NATIVE_LOGIN);
+            //availableAuthTypes.add(AuthType.KAKAO_TALK_EXCLUDE_NATIVE_LOGIN);
         }
         if(StoryProtocol.existCapriLoginActivityInStory(getReactApplicationContext(), Session.getCurrentSession().isProjectLogin())){
             availableAuthTypes.add(AuthType.KAKAO_STORY);
         }
-        availableAuthTypes.add(AuthType.KAKAO_ACCOUNT);
+
 
         final AuthType[] selectedAuthTypes = Session.getCurrentSession().getAuthTypes();
         availableAuthTypes.retainAll(Arrays.asList(selectedAuthTypes));
@@ -60,7 +69,7 @@ public class KakaoLoginManagerModule extends ReactContextBaseJavaModule implemen
         // 개발자가 설정한 것과 available 한 타입이 없다면 직접계정 입력이 뜨도록 한다.
         if(availableAuthTypes.size() == 0){
             availableAuthTypes.add(AuthType.KAKAO_ACCOUNT);
-        }
+        }*/
         return availableAuthTypes;
     }
 
@@ -73,29 +82,53 @@ public class KakaoLoginManagerModule extends ReactContextBaseJavaModule implemen
         ((TopActivityWrapper)getCurrentActivity().getApplication()).setTopActivity(getCurrentActivity());
 
         ISessionCallback sessionCallback = new ISessionCallback() {
-            //Promise promise;
-
             private void clear() {
                 Session.getCurrentSession().removeCallback(this);
             }
 
             @Override
             public void onSessionOpened() {
-                Boolean obj = true;
-
                 clear();
-                promise.resolve(obj);
+
+                UserManagement.requestMe(new MeResponseCallback() {
+
+                    @Override
+                    public void onSuccess(UserProfile result) {
+                        WritableMap map = Arguments.createMap();
+
+                        map.putString("nickname", result.getNickname());
+                        map.putString("profileImagePath", result.getProfileImagePath());
+                        map.putString("thumbnailImagePath", result.getThumbnailImagePath());
+                        map.putString("UUID", result.getUUID());
+
+                        map.putInt("remainingGroupMsgCount", result.getRemainingGroupMsgCount());
+                        map.putInt("remainingInviteCount", result.getRemainingInviteCount());
+
+                        map.putString("id", result.getId()+"");
+                        map.putString("serviceUserId", result.getServiceUserId()+"");
+
+                        promise.resolve(map);
+                    }
+
+                    @Override
+                    public void onSessionClosed(ErrorResult errorResult) {
+                        promise.reject(new Error(errorResult.getErrorMessage()));
+                    }
+
+                    @Override
+                    public void onNotSignedUp() {
+                        promise.reject(new Error("Not signed up."));
+                    }
+                });
             }
 
             @Override
             public void onSessionOpenFailed(KakaoException exception) {
-                if(exception != null) {
-                    //Logger.e(exception);
-                }
                 clear();
-                promise.reject(exception);
-                //
-                // setContentView(R.layout.layout_common_kakao_login);
+                if(exception != null) {
+                    promise.reject(exception);
+                }
+
             }
 
         };
@@ -103,7 +136,10 @@ public class KakaoLoginManagerModule extends ReactContextBaseJavaModule implemen
         Session.getCurrentSession().addCallback(sessionCallback);
 
         final List<AuthType> authTypes = getAuthTypes();
+        Log.d("checking", authTypes.size()+"");
+
         if(authTypes.size() == 1){
+            Log.d("checking", authTypes.get(0).name());
             Session.getCurrentSession().open(authTypes.get(0), getCurrentActivity());
         } else {
 
--- Example/index.android.js
+++ Example/index.android.js
@@ -6,48 +6,101 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Dimensions,
+    NativeModules
 } from 'react-native';
 
+const {
+    KakaoLoginManager
+} = NativeModules;
+
+import Button from 'react-native-button';
+import colors from 'material-colors';
+
 class Example extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    async handleClickKakaoButton(e) {
+        e.preventDefault();
+
+        try {
+            let resp = await KakaoLoginManager.login();
+
+            console.log(resp);
+        }
+
+        catch(e) {
+            console.log(e);
+
+        }
+
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button
+                    onPress={this.handleClickKakaoButton.bind(this)}
+                    containerStyle={styles.button}>
+                    <View style={styles.iconWrapper}>
+                        <Image source={require('./images/kakao_login_symbol.png')} style={{width:28, height: 25}}/>
+                    </View>
+
+
+                    <View  style={styles.textWrapper}>
+                        <View style={styles.textContainer}>
+                            <Text style={styles.text}>
+                                카카오톡으로 로그인
+                            </Text>
+                        </View>
+                    </View>
+                </Button>
+            </View>
+        );
+    }
 }
 
+const {width} = Dimensions.get('window');
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button: {
+        //alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.yellow[400],
+        borderRadius: 4,
+        height: 60,
+        elevation: 2,
+        width: width/4*3,
+        marginTop: 15
+    },
+    iconWrapper: {
+        flex: 3,
+        alignItems: 'center'
+    },
+    textWrapper: {
+        flex: 7,
+        borderLeftWidth: 3,
+        alignItems: 'center',
+        height: 40
+    },
+    textContainer: {
+        flex: 1,
+        //alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        fontFamily: 'NanumGothic-ExtraBold'
+    }
 });
 
 AppRegistry.registerComponent('Example', () => Example);
